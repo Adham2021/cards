cf3ed0ab74d4794ff465a94d2e0ae33969be7998
Adham shahwan <adham@fit-x.app>
2023-12-10 14:18:35 +0200

    format design pattern

--- server.js
+++ server.js
@@ -3,47 +3,15 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = 3000; // You can use any available port
+const path = require('path');
+
 
 // Body parser middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve your static files (like your HTML page)
-app.use(express.static('public'));
-
-// // POST endpoint to handle form submissions
-// app.post('/submit-form_main', async (req, res) => {
-//     console.log(req)
-//     try {
-//         const { name, phone, massage } = req.body;
-
-//         // Create a Nodemailer transporter
-//         const transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth: {
-//                 user: 'adham.shahwan94@gmail.com', // Your Gmail email address
-//                 pass: 'qypb goij naqg cfiq', // Your Gmail password (use an app password for security)
-//             },
-//         });
-
-//         // Email options
-//         const mailOptions = {
-//             from: 'adham.shahwan94@gmail.com',
-//             to: 'adham@fit-x.app',
-//             subject: `ליד חדש שמעוניין בשירות שלך`,
-//             text: `שם הלקוח: ${name}\nמספר טלפון: ${phone}\nהתוכן: ${massage}`,
-//         };
-
-//         // Send the email
-//         const info = await transporter.sendMail(mailOptions);
-
-//         // Respond to the client
-//         res.status(200).send('תודה רבה,הפרטים שלך נקלטו במערכת, נחזור אליך בהקדם!');
-//     } catch (error) {
-//         console.error('שגיאה בשליחת נתונים: ', error);
-//         res.status(500).send('שגיאה בשליחת נתונים');
-//     }
-// });
+app.use(express.static(path.join(__dirname, '/')));
 
 app.post('/submit-form_index', async (req, res) => {
     console.log(req)
