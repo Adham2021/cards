31981ccc55bc73ada0b66e0a2de3d93def7010d3
Adham shahwan <adham@fit-x.app>
2023-12-11 17:53:13 +0200

    make changes

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 // Serve your static files (like your HTML page)
 app.use(express.static(path.join(__dirname, '/')));
 
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
 app.post('/submit-form_index', async (req, res) => {
     console.log(req)
     try {
