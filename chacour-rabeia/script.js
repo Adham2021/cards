b278312b9f48f02ca12ee114f59321a7445d4eaf
Adham shahwan <adham@fit-x.app>
2024-01-20 13:54:42 +0200

    add rabiea shqqor

---
+++ chacour-rabeia/script.js
@@ -0,0 +1,403 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Call the function on page load
+    updateWorkingHoursStatus();
+});
+
+function shareOn(type) {
+    
+    const text = 'כרטיס הביקור הדיגטלי של רואה חשבון שקור רביע';
+    const url = window.location.href;
+    if (type === 'facebook') {
+        const facebookShareURL = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+        window.open(facebookShareURL, '_blank');
+    }
+    if (type === 'telegram') {
+
+        const telegramShareURL = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(text)}`;
+        window.open(telegramShareURL, '_blank');
+
+    }
+    if (type === 'messenger') {
+
+        const messengerShareURL = `https://www.facebook.com/dialog/send?link=${encodeURIComponent(url)}&app_id=123456789&redirect_uri=${encodeURIComponent(url)}&quote=${encodeURIComponent(text)}`;
+window.open(messengerShareURL, '_blank');
+    }
+    if (type === 'whatsapp') {
+
+        const whatsappShareURL = `https://api.whatsapp.com/send?text=${encodeURIComponent(text)} ${encodeURIComponent(url)}`;
+window.open(whatsappShareURL, '_blank');
+
+    }
+    if (type === 'linkedin') {
+        const linkedinShareURL = `https://www.linkedin.com/shareArticle?url=${encodeURIComponent(url)}&title=${encodeURIComponent(text)}`;
+        window.open(linkedinShareURL, '_blank');
+    }
+}
+changeLanguage(getLanguage(), false)
+
+function updateWorkingHoursStatus() {
+    const now = new Date();
+    const dayOfWeek = now.getDay(); // 0 is Sunday, 1 is Monday, etc.
+    const currentHour = now.getHours();
+    
+    const workingHours = {
+        0: currentHour >= 8 && currentHour < 20 ? 'Open until 17:30' : 'Closed, Open at 08:30', // Sunday
+        1: currentHour >= 8 && currentHour < 20 ? 'Open until 17:30' : 'Closed, Open at 08:30', // Monday
+        2: currentHour >= 8 && currentHour < 20 ? 'Open until 17:30' : 'Closed, Open at 08:30', // Tuesday
+        3: currentHour >= 8 && currentHour < 20 ? 'Open until 17:30' : 'Closed, Open at 08:30', // Wednesday
+        4: currentHour >= 8 && currentHour < 20 ? 'Open until 17:30' : 'Closed, Open at 08:30', // Thursday
+        5:  'Closed, Open at 08:30-Sunday', // Friday
+        6:  'Closed, Open at 08:30-Sunday', // Saturday
+    };
+
+    const statusOpenElement = document.getElementById('status-open');
+    const statusUntilElement = document.getElementById('status-until');
+
+    const currentLanguage = getLanguage();
+
+    if (workingHours[dayOfWeek].includes('Closed')) {
+        if (currentLanguage === 'hebrew') {
+            statusOpenElement.textContent = 'סגור';
+            statusUntilElement.textContent = ' נפתח ב- 08:30';
+        } else if (currentLanguage === 'arabic') {
+            statusOpenElement.textContent = 'مغلق';
+            statusUntilElement.textContent = ' يفتح في الساعة 08:30';
+        }
+    if(dayOfWeek==5 || dayOfWeek==6){
+        if (currentLanguage === 'hebrew') {
+            statusUntilElement.textContent += " יום א ";
+        }
+     else if (currentLanguage === 'arabic') {
+        statusUntilElement.textContent += " الاحد "
+     }
+    }
+        statusOpenElement.classList.add('status-close');
+        statusOpenElement.classList.remove('status-open');
+    } else {
+        if (currentLanguage === 'hebrew') {
+            statusOpenElement.textContent = 'פתוח';
+            statusUntilElement.textContent = ' עד 17:30';
+        } else if (currentLanguage === 'arabic') {
+            statusOpenElement.textContent = 'مفتوح';
+            statusUntilElement.textContent = ' حتى 17:30';
+        }
+
+        statusOpenElement.classList.add('status-open');
+        statusOpenElement.classList.remove('status-close');
+    }
+}
+
+
+
+function sendWhatsAppMessage(phone) {
+    const whatsappLink = document.getElementById("whatsapp-link");
+    const phoneNumber = phone;
+
+    // Replace 'YOUR_DEFAULT_MESSAGE' with your desired default message
+    const defaultMessage = encodeURIComponent(' שלום אפשר פרטים ?');
+
+    const whatsappApi = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${defaultMessage}`;
+    window.open(whatsappApi, '_blank');
+}
+function toggleContent(btn) {
+    // Toggle the 'show' class on the toggle-content div
+    var toggleContent = btn.nextElementSibling;
+    toggleContent.classList.toggle('show');
+
+    // Toggle the icon based on the 'show' class
+    var icon = btn.querySelector('i');
+    if (toggleContent.classList.contains('show')) {
+        icon.classList.remove('fa-plus');
+        icon.classList.add('fa-minus');
+    } else {
+        icon.classList.remove('fa-minus');
+        icon.classList.add('fa-plus');
+    }
+}
+function toggleService(btn) {
+    // Toggle the 'show' class on the toggle-content div
+    var toggleContent = btn.nextElementSibling;
+    toggleContent.classList.toggle('show');
+
+    // Toggle the icon based on the 'show' class
+    var icon = btn.querySelector('i');
+    if (toggleContent.classList.contains('show')) {
+        icon.classList.remove('fa-angle-down');
+        icon.classList.add('fa-angle-up');
+    } else {
+        icon.classList.remove('fa-angle-up');
+        icon.classList.add('fa-angle-down');
+    }
+}
+function addToContacts() {
+    var contact = {
+        name: "רואה חשבון שקור רביע",
+        phone: "+972527742312",
+        email: "R.cpa158@gmail.com"
+    };
+    // create a vcard file
+    var vcard = "BEGIN:VCARD\nVERSION:4.0\nFN:" + contact.name + "\nTEL;TYPE=work,voice:" + contact.phone + "\nEMAIL:" + contact.email + "\nEND:VCARD";
+    var blob = new Blob([vcard], { type: "text/vcard" });
+    var url = URL.createObjectURL(blob);
+
+    const newLink = document.createElement('a');
+    newLink.download = contact.name + ".vcf";
+    newLink.textContent = contact.name;
+    newLink.href = url;
+
+    newLink.click();
+}
+$(document).ready(function() {
+    $(".toggle-btn").click(function() {
+        $(this).next(".toggle-content").slideToggle("slow");
+        $(this).find("i").toggleClass("fa-angle-down fa-angle-up");
+    });
+});
+
+
+
+
+function changeLanguage(language, byClickButton = true) {
+    const languageData = {
+        hebrew: { flag: "/assets/images/files/flags/israel.png", text: "עברית" },
+        arabic: { flag: "/assets/images/files/flags/egypt.png", text: "العربية" },
+        english: { flag: "/assets/images/files/flags/united-kingdom.png", text: "English" },
+    };
+    const selectedLanguage = languageData[language];
+    localStorage.setItem('OranetPreferredLanguage', language);
+    document.getElementById("selected-language").innerText = selectedLanguage.text;
+    document.getElementById("language-menu-btn").getElementsByTagName("img")[0].src = selectedLanguage.flag;
+
+    // Additional logic to change content or perform actions based on the selected language
+    updateContent(language);
+    updateWorkingHoursStatus()
+
+
+    if (byClickButton) {
+        toggleLanguageMenu();
+    }
+}
+function getLanguage() {
+    return localStorage.getItem('OranetPreferredLanguage') || 'hebrew'; // Default language is Arabic
+}
+function toggleLanguageMenu() {
+    var languageMenu = document.getElementById("language-menu");
+    languageMenu.style.display = (languageMenu.style.display === "block") ? "none" : "block";
+}
+
+function updateContent(language) {
+    if(language=='arabic'){
+        $('body').css('font-family', 'Cairo, sans-serif');
+    }
+    if(language=='hebrew'){
+        $('body').css('font-family', 'Heebo, sans-serif');
+    }
+    const translatableElements = document.querySelectorAll('.translatable');
+
+    translatableElements.forEach(element => {
+        const translatedText = element.dataset[`lang${language.charAt(0).toUpperCase()}${language.slice(1)}`];
+        if (translatedText) {
+            if (element.tagName.toLowerCase() === 'p' || element.tagName.toLowerCase() === 'span') {
+                element.innerText = translatedText;
+            } else {
+                // Assuming other elements might have innerText as well
+                element.innerText = translatedText;
+            }
+        }
+    });
+}
+
+
+
+
+
+
+let currentReviewIndex = 0;
+const slider = document.getElementById('slider');
+const totalSlides = document.querySelectorAll('.slide').length;
+let intervalId; // Variable to store the interval ID
+
+function changeSlide(direction) {
+    currentReviewIndex = (currentReviewIndex + direction + totalSlides) % totalSlides;
+
+    const translateValue = currentReviewIndex * 100 + '%';
+    slider.style.transform = 'translate(' + translateValue + ')';
+}
+
+// Function to start the interval
+function startInterval() {
+    intervalId = setInterval(() => {
+        changeSlide(1);
+    }, 5000);
+}
+
+// Function to stop the interval
+function stopInterval() {
+    clearInterval(intervalId);
+}
+
+// Add event listeners to detect touch events on the slider
+slider.addEventListener('touchstart', stopInterval);
+slider.addEventListener('touchend', startInterval);
+
+// Start the initial interval
+startInterval();
+
+
+
+
+document.getElementById('scan-icon').addEventListener('click', function() {
+  
+    // Slide up the barcode dialog
+    document.getElementById('barcode-dialog').style.transform = 'translateY(0)';
+    document.getElementById('barcode-dialog').style.display = 'block';
+});
+
+document.getElementById('close-dialog').addEventListener('click', function() {
+    // Slide down the barcode dialog
+    document.getElementById('barcode-dialog').style.transform = 'translateY(100%)';
+});
+
+// Close the dialog when clicking outside
+document.addEventListener('click', function(event) {
+    const clickedElement = event.target;
+
+    if (clickedElement.className!=="fas fa-qrcode") {
+        // Clicked outside the open dialog
+        document.getElementById('barcode-dialog').style.transform = 'translateY(100%)';
+    }
+});
+
+// Prevent clicks inside the dialog from closing it
+document.getElementById('barcode-dialog').addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+
+
+$(document).ready(function() {
+    $("[data-fancybox]").fancybox({
+      // Options here
+      buttons: [
+        "zoom",
+        "slideShow",
+        "fullScreen",
+        "download",
+        "thumbs",
+        "close"
+      ]
+    });
+    function handleIntersection(entries, observer) {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('img-visible');
+            observer.unobserve(entry.target);
+          }
+        });
+      }
+  
+      // Set up the Intersection Observer
+      const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+  
+      // Target all images with the class 'img-responsive' inside the gallery
+      const images = document.querySelectorAll('#gallery .img-responsive');
+  
+      // Observe each image
+      images.forEach(image => {
+        observer.observe(image);
+      });
+  });
+
+  $(document).ready(function () {
+    const text1 = $('#text1');
+    const text2 = $('#text2');
+    const text3 = $('#text3'); // Add text3
+
+    const duration = 1000; // Duration for each text display
+
+    function toggleText() {
+        text1.fadeIn(duration, function () {
+            text1.delay(duration).fadeOut(duration, function () {
+                text2.fadeIn(duration, function () {
+                    text2.delay(duration).fadeOut(duration, function () {
+                        text3.fadeIn(duration, function () { // Fade in text3
+                            text3.delay(duration).fadeOut(duration, function () { // Fade out text3
+                                toggleText(); // Repeat the loop
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    }
+
+    toggleText(); // Start the loop
+});
+
+
+$(document).ready(function () {
+    const addToHomeScreenButton = $('#addToHomeScreenButton');
+    let deferredPrompt;
+
+    // Check for the beforeinstallprompt event and store it in deferredPrompt
+    window.addEventListener('beforeinstallprompt', (event) => {
+        event.preventDefault();
+        deferredPrompt = event;
+        addToHomeScreenButton.show(); // Show the "Add to Home Screen" button
+    });
+
+    // Add click event listener to the button
+    addToHomeScreenButton.click(() => {
+        if (deferredPrompt) {
+            // Show the installation prompt
+            deferredPrompt.prompt();
+            // Wait for the user's response
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log('User accepted the A2HS prompt');
+                } else {
+                    console.log('User dismissed the A2HS prompt');
+                }
+                deferredPrompt = null;
+                addToHomeScreenButton.hide(); // Hide the button after user interaction
+            });
+        }
+    });
+});
+
+$(document).ready(function() {
+    $("#emailForm").submit(function(event) {
+        event.preventDefault();
+        
+        const emailData = {
+            to: "adham@fit-x.app",
+            name: $("#name").val(),
+            phone: $("#phone").val(),
+            message: $("#message").val()
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "/send-email",
+            contentType: "application/json",
+            data: JSON.stringify(emailData),
+            success: function(data) {
+                if (data.success) {
+                    debugger;
+                    $("#successMessage").show();
+                    $("#errorMessage").hide();
+                    alert("תודה על ההודעה שלך")
+                } else {
+                    $("#successMessage").hide();
+                    $("#errorMessage").show();
+                }
+            },
+            error: function(error) {
+                $("#errorMessage").show();
+            }
+        });
+    });
+
+});
--- server.js
+++ server.js
@@ -160,6 +160,11 @@ app.get('/style.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'rizik', 'index.html'));
   });
 
+  app.use('/chacour-rabeia', express.static(path.join(__dirname, 'chacour-rabeia')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'chacour-rabeia', 'index.html'));
+  });
+
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
