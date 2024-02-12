a1d459c377362e62e245fe05ea4f002ca831b723
Adham shahwan <adham@fit-x.app>
2024-02-12 15:50:51 +0200

    update break-zone

--- break-zone/cart.js
+++ break-zone/cart.js
@@ -2,34 +2,58 @@
 let cart = [];
 
 // Define the addToCart function
-function addToCart(target,price,option) {
+function addToCart(target,price,dropdown) {
+    cartShake();
     // Find the parent meal div
     let mealDiv = $(target).closest('.meal');
-    
+
     // Extract meal information from data attributes
     let mealName = mealDiv.data('meal-name');
-    if(option != undefined && option!=null){
-        mealName =option;
+    if(dropdown != undefined && dropdown!=null){
+        var selectedOption = dropdown.options[dropdown.selectedIndex];
+        var selectedProduct = selectedOption.text;
+        mealName=selectedProduct;
     }
     let selectedContents = [];
 
-    let mealContents = mealDiv.find('input[type="checkbox"]:checked');
+    let mealContents = mealDiv.find('input[type="checkbox"]:not([name^="sauceMeal_"])');
+    let CheckedMealContents = mealDiv.find('input[type="checkbox"]:checked:not([name^="sauceMeal_"])');
 
-    mealContents.each(function() {
+    let sauceMealCheckboxes = mealDiv.find('input[type="checkbox"][name^="sauceMeal_"]:checked');
+
+    let AllContentsChecked= false;
+    let IsContentsNeeded=true;
+    let WithoutContentsMessage= "";
+
+    if(CheckedMealContents.length == mealContents.length){
+        AllContentsChecked=true;
+    }
+    if(CheckedMealContents.length==0){
+        IsContentsNeeded=false;
+        WithoutContentsMessage ="/بدون اي اضافات/";
+    }
+
+    
+if(!AllContentsChecked && IsContentsNeeded){
+    CheckedMealContents.each(function() {
         // Get the id of the current checkbox
         let contentId = $(this).attr('id'); 
 
         // Find the label associated with the checkbox using the 'for' attribute
         let contentName = $('label[for="' + contentId + '"]');
         selectedContents.push(contentName.text().trim());
-
-        let contentPrice = parseFloat($(this).attr('data-price'));
-        if(!isNaN(contentPrice)){
-            price+=contentPrice;
-            mealName += " \n(" + contentName.text().trim() + "),";
-        }
-
     });
+}
+
+sauceMealCheckboxes.each(function() {
+    // Get the id of the current checkbox
+    let contentId = $(this).attr('id'); 
+
+    // Find the label associated with the checkbox using the 'for' attribute
+    let contentName = $('label[for="' + contentId + '"]');
+    selectedContents.push(contentName.text().trim());
+});
+
     let quantity = parseInt(mealDiv.find('.quantity-input').val());
     if (isNaN(quantity) || quantity <= 0) {
         alert('Invalid quantity! Please enter a valid quantity.');
@@ -41,7 +65,9 @@ function addToCart(target,price,option) {
         name: mealName,
         contents: selectedContents,
         quantity: quantity,
-        price:price
+        price:price,
+        IsContentsNeeded:IsContentsNeeded,
+        WithoutContentsMessage:WithoutContentsMessage
     };
 
 
@@ -51,6 +77,16 @@ function addToCart(target,price,option) {
     $('#items-cart-num').text(cart.length);
     // Log the cart for debugging
     console.log('Cart:', cart);
+}
+
+function cartShake(){
+    var cart = document.getElementById('cart');
+    setTimeout(function() {
+        cart.classList.add('shake');
+        setTimeout(function() {
+            cart.classList.remove('shake');
+        }, 500);
+    }, 0);
 }
 
 // Global function to increase quantity
@@ -102,13 +138,15 @@ function renderCart() {
             // Render each item in the cart
             let totalPrice = item.price * item.quantity;
             totalCartPrice += totalPrice; // Add the current item's total price to the totalCartPrice
-            
+            let contents = item.contents.map(content => `+ ${content}`).join('\n');
+            console.log(contents)
             // Render meal item with improved styling
             cartItems.append(`
                 <div class="cart-item" style="border-bottom: 1px solid #eee; padding: 15px; position: relative;">
                     <button class="btn btn-danger delete-item" data-index="${index}" style="position: absolute; top: 5px; left: 5px;"><i class="fas fa-trash-alt"></i></button>
-                    <p><strong style="color: #333; font-size: 18px;">${item.name}</strong></p>
-                    <div >
+                    <p style="margin-bottom: 0;"><strong style="color: #333; font-size: 18px;">${item.name}</strong></p>
+                    <p style="color: gray; font-size: 13px;">${contents}</p>
+                    <div>
                         כמות (الكمية):
                         <button class="change-quantity" data-index="${index}" data-change="decrease"><i class="fas fa-minus"></i></button>
                         <span style="font-size: 16px;border:1px solid;border-radius: 2px;padding: 5px;">${item.quantity}</span>
@@ -130,6 +168,9 @@ function renderCart() {
                 <div style="margin-bottom: 5px;">
                     <input type="tel" class="form-control" id="phone" placeholder="הזן את מספר הטלפון שלך (ادخل رقم الهاتف)" required>
                 </div>
+                <div style="margin-bottom: 5px;">
+                <input type="text" class="form-control" id="address" placeholder="הזן את כתובתך (ادخل عنوانك الكامل)" required>
+            </div>
                 <button onclick="checkoutOrder()" class="btn btn-success" style="font-size: 18px;margin-bottom: 20px;">תזמין (اطلب الان)</button>
                 
             </div>
@@ -174,7 +215,7 @@ function updateTotalCartPrice() {
 
 
 // Bind click event to cart icon
-$('#cartIcon').click(function() {
+$('#cart').click(function() {
     renderCart(); // Render cart contents when clicked
     $('#cartModal').modal('show'); // Show the modal
 });
@@ -189,11 +230,12 @@ $(document).on('click', '.delete-item', function() {
 
 // Function to send the order via WhatsApp
 // Function to send the order via WhatsApp
-function sendOrderViaWhatsApp(name, phone, cart) {
+function sendOrderViaWhatsApp(name, phone,address, cart) {
     // Construct the message body
     let messageBody = `*وصلتك طلبية جديدة - הזמנה חדשה!*\n\n`;
     messageBody += `الاسم:  ${name}\n`;
-    messageBody += `الهاتف:  ${phone}\n\n`;
+    messageBody += `الهاتف:  ${phone}\n`;
+    messageBody += `العنوان:  ${address}\n\n`;
     messageBody += `--------------\n`;
     messageBody += `تفاصيل الطلبية:\n\n`;
     // Iterate over each item in the cart
@@ -201,10 +243,15 @@ function sendOrderViaWhatsApp(name, phone, cart) {
         
         let itemTotalPrice = item.price * item.quantity;
         // Add meal name, contents, quantity, and total price to the message
-        messageBody += `*${index+1}.${item.name}*\n`;
-        messageBody += `الاضافات: ${item.contents.join(', ')}\n`;
-        messageBody += `سعر الوجبة: ${item.price} ₪\n`;
-        messageBody += `الكمية: ${item.quantity}\n`;
+        messageBody += `*${index+1}.${item.name}* X${item.quantity}\n`;
+        messageBody += 'الاضافات:\n';
+        if(item.IsContentsNeeded || item.contents.length>0){            
+            messageBody += item.contents.map(content => `+ ${content}`).join('\n');
+        }
+        else { 
+            messageBody += `${item.WithoutContentsMessage}\n`;
+        }
+        messageBody += `\nسعر الوحدة: ${item.price} ₪\n`;
         messageBody += `السعر الكلي: ${itemTotalPrice} ₪\n\n`;
         messageBody += `--------------\n\n`;
     });
@@ -230,6 +277,7 @@ function checkoutOrder() {
     // Get the input values
     var name = $('#name').val().trim();
     var phone = $('#phone').val().trim();
+    var address = $('#address').val().trim();
 
     // Check if name and phone are not empty
     if (name === '' || phone === '') {
@@ -241,12 +289,30 @@ function checkoutOrder() {
             alert('אנא הזן מספר טלפון חוקי.'); // Please enter a valid phone number.
         } else {
             // Display confirmation dialog
-            var confirmMessage = 'האם אתה בטוח שברצונך להמשיך להזמין ב-WhatsApp?';
+            var confirmMessage = 'האם אתה בטוח שברצונך להמשיך להזמין ב- WhatsApp?';
             if (confirm(confirmMessage)) {
                 // Proceed to the WhatsApp order page
                 // Send the order via WhatsApp
-                sendOrderViaWhatsApp(name, phone, cart);
+                sendOrderViaWhatsApp(name, phone,address, cart);
             }
         }
     }
 }
+
+$(document).ready(function() {
+    // Function to restrict checkboxes to allow only one selection within a group
+    function restrictCheckboxes(groupSelector) {
+      $(groupSelector).change(function() {
+        if ($(this).prop('checked')) {
+          $(groupSelector).not(this).prop('checked', false);
+        }
+      });
+    }
+
+    var numberOfMeals=8;
+    for(let i=1;i<numberOfMeals;i++){
+      var mealSelector = 'input[type="checkbox"][name="sauceMeal_' + i + '"]';
+      restrictCheckboxes(mealSelector);
+    }
+    
+  });
