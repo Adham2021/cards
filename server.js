d12b8b77c065cd8ec5360abce570f8a6e3c2a73c
Adham shahwan <adham@fit-x.app>
2024-03-03 18:05:56 +0200

    Add Emissa Perfumes

--- server.js
+++ server.js
@@ -215,6 +215,11 @@ app.get('/style.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'menu', 'index.html'));
   });
 
+  app.use('/emissa', express.static(path.join(__dirname, 'emissa')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'emissa', 'index.html'));
+  });
+
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, '404.html'));
