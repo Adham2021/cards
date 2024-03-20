27ef473998662c1895351a241d7617c93b34242d
Adham shahwan <adham@fit-x.app>
2024-03-20 16:26:54 +0200

    Add Lubna

--- server.js
+++ server.js
@@ -225,6 +225,10 @@ app.get('/style.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'dr.nutella', 'index.html'));
   });
 
+  app.use('/lubna', express.static(path.join(__dirname, 'lubna')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'lubna', 'index.html'));
+  });
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
