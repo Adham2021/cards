a691443bf39d78b97659ac5712e6a9c31d613298
Adham shahwan <adham@fit-x.app>
2024-03-23 11:46:48 +0200

    add sky of jerusalem

--- server.js
+++ server.js
@@ -229,6 +229,12 @@ app.get('/style.css', (req, res) => {
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'lubna', 'index.html'));
   });
+
+  app.use('/sky-of-jerusalem', express.static(path.join(__dirname, 'sky-of-jerusalem')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sky-of-jerusalem', 'index.html'));
+  });
+
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
