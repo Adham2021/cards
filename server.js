29571f956feab5d84ea7ccaade8144be7857afbd
Adham shahwan <adham@fit-x.app>
2024-03-27 11:09:48 +0200

    add omaima

--- server.js
+++ server.js
@@ -237,6 +237,11 @@ app.get('/style.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'sky-of-jerusalem', 'index.html'));
   });
 
+  app.use('/omaima', express.static(path.join(__dirname, 'omaima')));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'omaima', 'index.html'));
+  });
+
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
